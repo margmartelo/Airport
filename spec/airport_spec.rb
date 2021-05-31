e392a7b2d6c42093a7d611d2e11d0678acab1e9d
Margarida Martelo <margmartelo@gmail.com>
2021-05-31 20:29:42 +0100

    Testing that airport allows a plane to land

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,18 @@
 #So I can get passengers to a destination 
 #I want to instruct a plane to land at an airport
 require 'airport'
+require 'plane'
 
 describe Airport do
     it "should allow landing" do
         expect(subject).to respond_to :land
     end
+
+    it "should allow a plane to land" do
+        subject.land(Plane.new)
+        expect(subject.planes.length).to eq 1
+    end
+
 end
 
 
