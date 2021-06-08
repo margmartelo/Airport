11f2d4391833a77b02870af2b80b79e574dad284
Margarida Martelo <margmartelo@gmail.com>
2021-06-08 21:23:35 +0100

    Refactoring and mocking

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,31 +3,53 @@
 #I want to prevent landing when weather is storm
 
 require 'airport'
-require 'plane'
-
 
 describe Airport do
-    it "should allow landing" do
-        expect(subject).to respond_to :land
+    let(:plane) { double :plane }
+    describe "#land" do
+        context "when not stormy" do
+            before do 
+                allow(subject).to receive(:stormy?).and_return(false)
+            end
+
+            it "should allow a plane to land" do
+                subject.land(plane)
+                expect(subject.planes.length).to eq 1
+            end
+
+            context "when full capacity reached" do
+                it "should raise an error if trying to land " do
+                    subject.capacity.times { subject.land(plane) }
+                    expect { subject.land(plane) }.to raise_error("Landing is not possible: airport at full capacity.")
+                end
+            end
+        end
+
+        context "when stormy" do
+            it "should raise an error if trying to land" do
+                allow(subject).to receive(:stormy?).and_return(true)
+                expect { subject.land(plane) }.to raise_error("Due to stormy weather, it is not possible to land at the airport.")
+            end
+        end
     end
 
-    it "should allow a plane to land" do
-        allow(subject).to receive(:stormy?).and_return(false)
-        subject.land(Plane.new)
-        expect(subject.planes.length).to eq 1
+    describe "#take_off" do 
+        context "when not stormy" do
+            it "should allow a plane to take off" do
+                allow(subject).to receive(:stormy?).and_return(false)
+                3.times { subject.land(plane) }
+                subject.take_off
+                expect(subject.planes.length).to eq 2
+            end
+        end
+        
+        context "when stormy" do
+            it "shouldn't allow to take off" do
+                allow(subject).to receive(:stormy?).and_return(true)
+                expect { subject.take_off }.to raise_error("Due to stormy weather, it is not possible to take off.")
+            end
+        end
     end
-
-    it "should raise an error if trying to land at the airport with full capacity reached" do
-        allow(subject).to receive(:stormy?).and_return(false)
-        subject.capacity.times { subject.land(Plane.new) }
-        expect { subject.land(Plane.new) }.to raise_error("Landing is not possible: airport at full capacity.")
-    end
-
-    it "should raise an error if trying to land when stormy weather" do
-        allow(subject).to receive(:stormy?).and_return(true)
-        expect { subject.land(Plane.new) }.to raise_error("Due to stormy weather, it is not possible to land at the airport.")
-    end
-
 
     it "should allow to set the capacity of an airport" do
         new_airport = Airport.new(30)
@@ -39,17 +61,7 @@ describe Airport do
         expect(new_airport.capacity).to eq Airport::DEFAULT_CAPACITY
     end
 
-    it "should allow a plane to take off if weather isn't stormy" do
-        allow(subject).to receive(:stormy?).and_return(false)
-        3.times { subject.land(Plane.new) }
-        subject.take_off
-        expect(subject.planes.length).to eq 2
-    end
-
-    it "shouldn't take off if weather is stormy" do
-        allow(subject).to receive(:stormy?).and_return(true)
-        expect { subject.take_off }.to raise_error("Due to stormy weather, it is not possible to take off.")
-    end
+  
 end
 
 
